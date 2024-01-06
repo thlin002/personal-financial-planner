c32d6df843ce4e91deeb055c30190fbfb1b079cb
Lin_Tzu_Hsiang <thlin002@gmail.com>
2024-01-06 11:00:44 +0800

    feat: replace account entry CLI with reading xlsx file
    User will entry the account data in the xlsx file using external apps.
    If the program found out there's no existing xlsx file, it will create one for the user.

--- project.py
+++ project.py
@@ -9,26 +9,20 @@ from decimal import *
 def main():
     pd.set_option('display.max_rows', None)
     bank_records = Bank_records(Statement_analyzer("TransactionHistory_2023-Mar-Jul.csv").reader())
-    
-    savings = Savings('savings.csv', Decimal(25388))
-    print(get_bond_yields())
-    non_recurring_revenue = Non_recurring_CF('onetime.csv')
     spending = Decimal(bank_records.monthly_spending().mean()[0]).quantize(Decimal('.01'))
     income = Decimal(bank_records.monthly_income().min()[0])
-    recurring_cf = Recurring_CF('recurring.csv', spending=spending, income=income)
-    savings = Savings('savings.csv')
-    non_recurring_revenue.usr_book_writer()
-    recurring_cf.usr_book_writer()
-    savings.usr_book_writer()
-    
-    pd.set_option('display.max_rows', None)
     print(bank_records)
     print(bank_records.sum_by_category(monthly=True))
     print(bank_records.monthly_spending())
     print(bank_records.monthly_spending().mean())
     print(bank_records.monthly_income().min())
     print(bank_records.income_report())
-        
+
+    print(get_bond_yields())
+
+    future_cf = Future_CF('Future_cashflows.xlsx')
+    for item in future_cf.df.items():
+        print(item)
 
 def get_bond_yields():
     base_url = 'https://api.fiscaldata.treasury.gov/services/api/fiscal_service'
